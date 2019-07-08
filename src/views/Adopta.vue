33d726ba35f7ebf9f9404edf92f76e7fdf8a0123
flacket <facundoggallardo@gmail.com>
2019-07-08 17:47:56 -0300

    adopta

--- src/views/Adopta.vue
+++ src/views/Adopta.vue
@@ -2,35 +2,18 @@
   <div>
     <h1 class="display-1 primary--text font-weight-medium">Adopta</h1>
 
-    <image-uploader
-      :preview="true"
-      :className="['fileinput', { 'fileinput--loaded': hasImage }]"
-      capture="environment"
-      :debug="0"
-      :autoRotate="true"
-      outputFormat="blob"
-      :maxWidth="250"
-      :maxHeight="250"
-      :quality="0.9"
-      accept=".jpg"
-      @input="setImage"
-    >
-      <label for="fileInput" slot="upload-label">
-        <figure>
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-            <path
-              class="path1"
-              d="M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z"
-            />
-          </svg>
-        </figure>
-        <span class="upload-caption">
-          {{
-          hasImage ? "Reemplazar" : "Seleccionar foto de perfil"
-          }}
-        </span>
-      </label>
-    </image-uploader>
+          <v-btn class="primary" @click="selectImage">Subir Imagen</v-btn>
+          <input
+            type="file"
+            style="display: none"
+            ref="fileInput"
+            accept="image/*"
+            @change="selectedImage"
+          />
+          <v-img style="background: grey" :src="imageUrl" width="250" height="250"></v-img>
+
+<p v-if="loading" class="mt-4">Registrando mascota, espera mientras le ponemos la correa..</p>
+    <v-progress-linear v-if="loading" v-model="loadingProgress"></v-progress-linear>
 
     <v-btn @click="upload">Subir</v-btn>
     <!--
@@ -49,23 +32,39 @@ export default {
     return {
       loading: true,
       loadingProgress: 0,
-      hasImage: false,
-      image: ""
+      imageUrl: '',
+      image: null,
     };
   },
   methods: {
-    setImage: function(file) {
-      this.hasImage = true;
-      this.image = file;
-      console.log(this.image);
+    selectImage() {
+      this.$refs.fileInput.click();
+    },
+    selectedImage(e) {
+      const files = e.target.files;
+      let filename = files[0].name;
+      if (filename.lastIndexOf('.') <= 0) {
+        return alert('Por favor agrega un archivo valido!');
+      } else {
+        const fileReader = new FileReader();
+        fileReader.addEventListener('load', () => {
+          this.imageUrl = fileReader.result;
+          console.log('ImageUrl: ', this.imageUrl);
+        });
+        fileReader.readAsDataURL(files[0]);
+        this.image = files[0];
+        console.log('image: ', this.image);
+      }
     },
     upload() {
+        //Filepath = Direccion de guardado + Id del usuario + extension del archivo (.jpg)
+        //var filePath = 'usuarios/' + userId + this.image.name.slice(this.image.name.lastIndexOf('.'));
       var filePath = "usuarios/" + "test2" + ".jpg";
       console.log(filePath);
       //creo la referencia de donde se almacenara en GoogleStorage con el filePath
       var storageRef = firebaseApp.storage().ref(filePath);
       //guardo la imagen
-      var task = storageRef.put(this.image);
+      var task = storageRef.put(this.imageUrl);
       var self = this;
 
       task.on(
